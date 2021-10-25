2f57269c7076a49d46e6e3ed7487ea76ef1fcbc1
Alejandro <ilextron@gmail.com>
2021-10-25 13:22:27 +0200

    exercise1

---
+++ Practice-1.js
@@ -0,0 +1,14 @@
+class Person{
+    constructor(Name, LastName, Hair, Height, KnowledgeInProgramming, Programminglanguages){
+        this.Name = Name
+        this.LastName = LastName
+        this.Hair = Hair
+        this.Height = Height
+        this.KnowledgeInProgramming = "Nothing"
+        this.Programminglanguages = "Nothing"
+    }
+
+}
+
+const person1 = new Person("Andrea", "Taza", "1,65")
+console.log(person1)
