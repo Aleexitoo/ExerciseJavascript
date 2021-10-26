75d0f8ab556ceb8ed0e2cea9cf03079ed3f5a213
Alejandro <ilextron@gmail.com>
2021-10-26 14:27:25 +0200

    EjercicioGood

---
+++ Practice-1bien.js
@@ -0,0 +1,45 @@
+// NOMBRE Y APELLIDOS DE LA PERSONA
+const firstName = "Alejandro";
+console.log(firstName)
+const LastName = "Madueño";
+console.log(LastName)
+
+
+// EL PELO PASA DE ESTAR LISO A ESTA RIZADO
+let hair = "liso";
+let hair1 = "rizado";
+const hairComplete = `Mi pelo estaba ${hair}, pero ahora me lo he ${hair1}`;
+console.log(hairComplete)
+
+
+// MI ALTURA NORMAL Y MI ALTURA CON TACONES 
+let altura = 1.14;
+let MiAltura = "Mi altura es de " + [altura];
+console.log(MiAltura)
+let tacones = 0.30;
+const AlturaConTacones = altura + tacones;
+const AlturaTotal = `Mido ${altura}, pero con tacones mido ${AlturaConTacones}`;
+console.log(AlturaConTacones)
+console.log(AlturaTotal)
+
+
+// MI NOMBRE COMPLETO ES ALEJANDRO MADUEÑO
+const NombreCompleto = firstName + " " + LastName;
+const NombreCompleto1 = `Mi nombre completo es ${NombreCompleto}`;
+console.log(NombreCompleto1)
+
+
+// NO SE PROGRAMAR, NI USAR NINGUN LEGUAJE DE PROGRAMACION
+const programar = false;
+let Conocimiento = "Conociemiento en programacion:" + [programar];
+console.log(Conocimiento)
+let lenguaje = "No se usar ningun lenguaje de programacion";
+console.log(lenguaje)
+const NoSe = `Mi nivel de programacion es ${programar} y ${lenguaje}`;
+
+
+// AHORA SE PROGRAMAR Y SE LOS LENGUAJES JAVASCRIPT, PYTHON Y HTML
+const NivelProgramacion = true
+const TiposDeLenguaje = ["JavasScript", "Python", "HTML",];
+const Se = `${NoSe}, pero ahora puedo utilizar ${TiposDeLenguaje} y mi nivel de programacion es: ${NivelProgramacion}`;
+console.log(Se)
